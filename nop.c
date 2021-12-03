1d05a84004b3cf5163bc03147197b29edb3e4bc5
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2021-12-03 17:11:46 +0000

    Inactive.

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: double pointer to stack
+ * @line_no: current position in source file
+ * Return: nothing
+ */
+void nop(stack_t **stack, unsigned int line_no)
+{
+  (void) stack;
+  (void) line_no;
+}
